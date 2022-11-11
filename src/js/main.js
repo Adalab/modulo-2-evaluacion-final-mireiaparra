357ce875e6046c57447ad98c8f66cd6fb4f91b4b
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-11-11 14:59:46 +0100

    add fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,54 @@
 "use strict"
 
-function createChElement() {
-    for (let i=0; i<6; i++) {
-        const liElement = document.createElement("li");
+
+// function createChElement() {
+//     for (let i=0; i<6; i++) {
+//         const liElement = document.createElement("li");
        
-        const imgElement = document.createElement("img");
-        liElement.appendChild(imgElement);
-        const titleElement = document.createElement("h2");
-        liElement.appendChild(titleElement);
-        const textElement = document.createElement("p");
-        liElement.appendChild(textElement);
+//         const imgElement = document.createElement("img");
+//         liElement.appendChild(imgElement);
+//         const titleElement = document.createElement("h2");
+//         liElement.appendChild(titleElement);
+//         const textElement = document.createElement("p");
+//         liElement.appendChild(textElement);
 
-        allList.appendChild(liElement);
-    }
-}
+//         allList.appendChild(liElement);
 
-createChElement();
+     
+//     }
+//     return allList;
+// }
+
+// createChElement();
 
 function getCh(){
     fetch("https://breakingbadapi.com/api/characters")
     .then((response) => response.json())
     .then((data) => {
+        for (let i=0; i<8; i++) {
+            const liElement = document.createElement("li");
+       
+            const imgElement = document.createElement("img");
+            liElement.appendChild(imgElement);
+            const titleElement = document.createElement("h2");
+            liElement.appendChild(titleElement);
+            const textElement = document.createElement("p");
+            liElement.appendChild(textElement);
+    
+            allList.appendChild(liElement);
 
+           const nameText = document.createTextNode(`${data[i].name}`);
+           titleElement.appendChild(nameText);
+
+           imgElement.src = `${data[i].img}`;
+           imgElement.style.height = "150px";
+           imgElement.style.width = "110px";
+
+           const statusText = document.createTextNode(`${data[i].status}`);
+           textElement.appendChild(statusText);
+        }
+       
     });
 }
+
+getCh();
